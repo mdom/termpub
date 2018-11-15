e5c61c23d26b715d28b61933c532ba884350c01b
Mario Domgoergen <mdom@taz.de>
2018-11-15 20:49:05 +0100

    Add book reading

--- script/termpub.pl
+++ script/termpub.pl
@@ -1,57 +1,89 @@
 #!/usr/bin/perl
 
-use strict;
-use warnings;
-use Archive::Zip qw(:ERROR_CODES);
+package Epub;
+use Mojo::Base -base;
 use Mojo::DOM;
 use Mojo::Util qw(decode encode html_unescape);
-use Curses;
-use POSIX qw(ceil);
+use Archive::Zip qw(:ERROR_CODES);
 
-Archive::Zip::setErrorHandler( sub { } );
+has 'filename';
 
-my @epubs = @ARGV;
-
-my @books;
-
-for my $epub (@epubs) {
-    my $archive = eval { Archive::Zip->new };
-    if ( eval { $archive->read($epub) } != AZ_OK ) {
-        warn "Can't read $epub\n";
-        next;
+has archive => sub {
+    my $self    = shift;
+	my $filename = $self->filename;
+    my $archive = Archive::Zip->new;
+    if ( eval { $archive->read($filename) } != AZ_OK ) {
+        die "Can't read $filename\n";
     }
     my $mimetype = $archive->contents('mimetype');
     $mimetype =~ s/[\r\n]+//;
     if ( !$mimetype ) {
-        warn "Missing mimetype for $epub: skipping\n";
-        next;
+        die "Missing mimetype for $filename\n";
     }
     if ( $mimetype ne 'application/epub+zip' ) {
-        warn "foo foo Unknown mimetype $mimetype for $epub: skipping\n";
-        next;
+        die "Unknown mimetype $mimetype for $filename\n";
     }
+    return $archive;
+};
 
-    my $container     = $archive->contents('META-INF/container.xml');
+has root_file => sub {
+	my $self = shift;
+    my $filename      = $self->filename;
+    my $container     = $self->archive->contents('META-INF/container.xml');
     my $container_dom = Mojo::DOM->new($container);
     my $root_file = $container_dom->at('rootfiles rootfile')->attr("full-path");
-
     if ( !$root_file ) {
-        warn "No root file defined for $epub\n";
-        next;
+        die "No root file defined for $filename\n";
     }
+    return $root_file;
+};
 
-    my $root = Mojo::Util::decode 'UTF-8', $archive->contents($root_file);
+has root_dom => sub {
+	my $self = shift;
+    my $root = Mojo::Util::decode 'UTF-8', $self->archive->contents($self->root_file);
     if ( !$root ) {
-        warn "Missing root file $root_file for $epub\n";
-        next;
+        die "Missing root file " . $self->root_file ." for " . $self->filename . "\n";
     }
-    my $root_dom = Mojo::DOM->new($root);
-    my $creator = eval { $root_dom->at('metadata')->at('dc\:creator')->content }
-      || 'Unknown';
-    my $title =
-      eval { $root_dom->at('metadata')->at('dc\:title')->content } || 'Unknown';
+    return Mojo::DOM->new($root);
+};
 
-    push @books, encode 'UTF-8', html_unescape "$creator - $title";
+has creator => sub {
+	my $self = shift;
+	return html_unescape( eval { $self->root_dom->at('metadata')->at('dc\:creator')->content } || 'Unknown');
+};
+
+has title => sub {
+	my $self = shift;
+	return html_unescape( eval { $self->root_dom->at('metadata')->at('dc\:title')->content } || 'Unknown');
+};
+
+package main;
+
+use strict;
+use warnings;
+use Mojo::DOM;
+use Mojo::File;
+use Mojo::Util qw(decode encode html_unescape);
+use Curses;
+use POSIX qw(ceil);
+use Text::Wrap;
+
+Archive::Zip::setErrorHandler( sub { } );
+
+my @filenames = @ARGV;
+
+my @epubs;
+
+for my $filename (@filenames) {
+
+	my $epub = eval { Epub->new( filename => $filename ) };
+
+	if ( !$epub ) {
+		warn "$@\n";
+		next;
+	}
+
+    push @epubs, $epub;
 }
 
 initscr;
@@ -60,52 +92,81 @@ cbreak;
 keypad(1);
 curs_set(0);
 
-scroll_window(@books);
+my $position = 0;
+while (1) {
+	my @lines = map { encode 'UTF-8', $_->creator . " - " . $_->title } @epubs;
+    my $position = select_line( $position, @lines );
+    if ( not defined $position ) {
+        last;
+    }
+    read_book( $epubs[$position] );
+}
 
 END {
     endwin;
 }
 
-sub scroll_window {
-    my @lines = @_;
+sub read_book {
+    my $epub = shift;
+
+	my $root_file = Mojo::File->new( $epub->root_file );
+
+    my @idrefs = $epub->root_dom->find('spine itemref')->map( attr => 'idref' )->each;
+    my $chapter_selector = join( ',', map { "manifest item#$_" } @idrefs );
+    my @chapter_files =
+      $epub->root_dom->find($chapter_selector)
+      ->grep( sub { $_->attr('media-type') eq 'application/xhtml+xml' } )
+      ->map( attr => 'href' )->map( sub { $root_file->sibling($_)->to_rel } )->each;
+	  clear;
+	  move(0,0);
+    addstring( render_content( decode('UTF-8', $epub->archive->contents( "".$chapter_files[4] )) ));
+	refresh;
+	getch;
+}
+
+sub select_line {
+    my ( $position, @lines ) = @_;
     my ( $rows, $columns );
     getmaxyx( stdscr, $rows, $columns );
     my $len      = @lines;
-    my $position = 0;
     my $page     = 0;
     my $max_page = ceil( @lines / $rows ) - 1;
 
-	my $changed_page = 1;
-	my @current_lines;
-	my $old_page = -1;
-	my $old_position = $position;
+    my $changed_page = 1;
+    my @current_lines;
+    my $old_page     = -1;
+    my $old_position = $position;
 
     while (1) {
-		if ($old_page != $page ) {
-			@current_lines = @lines[ ($page * $rows) .. ($page == $max_page ? $#lines : ($page*$rows+$rows-1))  ];
-			$changed_page = 0;
-			clear;
-			move( 0, 0 );
-			addstring(join("\n",@current_lines));
-			$old_page = $page;
-		}
+        if ( $old_page != $page ) {
+            @current_lines =
+              @lines[ ( $page * $rows )
+              .. (
+                  $page == $max_page ? $#lines : ( $page * $rows + $rows - 1 ) )
+              ];
+            $changed_page = 0;
+            clear;
+            move( 0, 0 );
+            addstring( join( "\n", @current_lines ) );
+            $old_page = $page;
+        }
 
-		if ( $old_position != $position ) {
-			move ( $old_position, 0 );
-			clrtoeol;
-			addstring($current_lines[$old_position]."\n");
-			$old_position = $position;
-		}
+        if ( $old_position != $position ) {
+            move( $old_position, 0 );
+            clrtoeol;
+            addstring( $current_lines[$old_position] . "\n" );
+            $old_position = $position;
+        }
 
-		move($position,0);
-		clrtoeol;
-		attron(A_REVERSE);
-		addstring($current_lines[$position]."\n");
-		attroff(A_REVERSE);
+        move( $position, 0 );
+        clrtoeol;
+        attron(A_REVERSE);
+        addstring( $current_lines[$position] . "\n" );
+        attroff(A_REVERSE);
 
         refresh;
 
-        my $c = getch;
+        my $c = getchar;
         if ( $c == KEY_NPAGE ) {
             if ( $page < $max_page ) {
                 $position = 0;
@@ -118,32 +179,91 @@ sub scroll_window {
                 $page--;
             }
         }
-		elsif ( $c == KEY_DOWN ) {
-			if ( $position < @current_lines - 1 ) {
-				$position++
-			}
-			else {
-				if ( $page != $max_page ) {
-					$position = 0;
-					$page++;
-				}
-			}
-		}
-		elsif ( $c == KEY_UP ) {
-			if ( $position != 0 ) {
-				$position--;
-			}
-			else {
-				if ( $page != 0 ) {
-					$position = $rows -1;
-					$page--;
-				}
-			}
-		}
+        elsif ( $c == KEY_DOWN ) {
+            if ( $position < @current_lines - 1 ) {
+                $position++;
+            }
+            else {
+                if ( $page != $max_page ) {
+                    $position = 0;
+                    $page++;
+                }
+            }
+        }
+        elsif ( $c == KEY_UP ) {
+            if ( $position != 0 ) {
+                $position--;
+            }
+            else {
+                if ( $page != 0 ) {
+                    $position = $rows - 1;
+                    $page--;
+                }
+            }
+        }
+        elsif ( $c eq "\n" ) {
+            return $position;
+        }
         elsif ( $c eq 'q' ) {
-            exit(0);
+            return;
         }
     }
 
     return;
 }
+
+sub infobox {
+    my $text = shift;
+    my $win = newwin( 20, 20, 5, 5 );
+    border(
+        $win,      ACS_VLINE,    ACS_VLINE,    ACS_HLINE,
+        ACS_HLINE, ACS_ULCORNER, ACS_URCORNER, ACS_LLCORNER,
+        ACS_LRCORNER
+    );
+    move( $win, 1, 1 );
+    addstring( $win, $text );
+    refresh($win);
+    getch;
+    delwin($win);
+    touchwin(stdscr);
+    refresh;
+    return;
+}
+
+sub render_content {
+    my ($item) = shift;
+    return fill( '  ', '  ', render_dom( Mojo::DOM->new( $item ) ) );
+}
+
+sub render_dom {
+    my $node    = shift;
+    my $content = '';
+    for ( $node->child_nodes->each ) {
+        $content .= render_dom($_);
+    }
+    if ( $node->type eq 'text' ) {
+        $content = $node->content;
+        $content =~ s/\.\s\.\s\./.../;
+        return '' if $content !~ /\S/;
+        return $content;
+    }
+    elsif ( $node->type eq 'tag' && $node->tag =~ /^h\d+$/
+        || is_tag( $node, 'p', 'div' ) )
+    {
+        return "$content ";
+    }
+    return $content;
+}
+
+sub is_tag {
+    my ( $node, @tags ) = @_;
+    return
+      if $node->type ne 'tag';
+    for my $tag (@tags) {
+        return 1
+          if $node->tag eq $tag;
+    }
+    return;
+}
+
+exit 0;
