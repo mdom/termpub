d8968f33540d0160262495f3d50c167c9d2fa2fa
Mario Domgoergen <mario@domgoergen.com>
2020-12-06 22:39:46 +0100

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author_email = 'mario@domgoergen.com',
     name = 'termpub',
     license = 'GPL-3.0',
-    version = '2020.12.02.2',
+    version = '2020.12.06.1',
     url = 'https://github.com/mdom/termpub',
     packages=["termpub"],
     entry_points = {
