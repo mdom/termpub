978a309bc682e2e27e9db0522dcb92afb93574fc
Mario Domgoergen <mario@domgoergen.com>
2020-12-22 22:46:09 +0100

    Use readline.Buffer in main_loop

--- termpub/readline.py
+++ termpub/readline.py
@@ -33,48 +33,18 @@ def readline(window, prompt=':', y=0, x=0):
     if left_pad + right_pad >= max_x:
         raise Exception('Window too small for readline().')
 
-    max_display  = max_x - left_pad - right_pad;
-
-    buffer_pos = 0
-    buffer = ''
+    buffer = Buffer()
 
     max_buffer_size = max_x - left_pad - right_pad
 
     while True:
 
-        if buffer_pos < 0:
-            buffer_pos = 0
+        slice, index = buffer.slice(max_buffer_size)
 
-        buffer_start = 0
-        cursor_pos = 0
-        correction = 0
-
-        for idx, c in enumerate(buffer[:buffer_pos]):
-            if not unicodedata.combining(c):
-                cursor_pos += 1
-                if cursor_pos % max_display == 0:
-                    buffer_start = next_buffer_pos(buffer,idx)
-                    correction = cursor_pos
-
-        buffer_end = buffer_start
-        visible = 0
-        for idx, c in enumerate(buffer[buffer_start:]):
-            buffer_end += 1
-            if not unicodedata.combining(c):
-                visible += 1
-                if visible >= max_buffer_size:
-                    break
-
-        for idx,c in enumerate(buffer[buffer_end:]):
-            if unicodedata.combining(c):
-                buffer_end += 1
-            else:
-                break
-
-        window.addstr( max_y - 1, 0, prompt + buffer[buffer_start:buffer_end])
+        window.addstr(max_y - 1, 0, prompt + slice)
         window.clrtoeol()
 
-        window.move( max_y - 1, cursor_pos + left_pad - correction )
+        window.move(max_y - 1, index + left_pad)
 
         c, printable = getkey(window)
 
@@ -89,60 +59,34 @@ def readline(window, prompt=':', y=0, x=0):
             raise ResizeEvent()
 
         elif c == 'KEY_LEFT':
-            buffer_pos = prev_buffer_pos(buffer, buffer_pos)
+            buffer.move_left()
 
         elif c == 'KEY_RIGHT':
-            buffer_pos = next_buffer_pos(buffer, buffer_pos)
+            buffer.move_right()
 
         elif c == 'KEY_HOME' or c == '^A':
-            buffer_pos = 0
+            buffer.move_to_start()
 
         elif c == '^K':
-            buffer = buffer[:buffer_pos]
+            buffer.delete_to_end()
 
         elif c == 'KEY_END' or c == '^E':
-            buffer_pos = len(buffer)
+            buffer.move_to_end()
 
         elif c == 'KEY_BACKSPACE':
-            p = buffer_pos
-            buffer_pos = prev_buffer_pos(buffer, buffer_pos)
-            buffer = buffer[:buffer_pos] + buffer[p:]
+            buffer.delete_backward()
 
         elif c == "^D":
-            p = buffer_pos
-            buffer_pos = next_buffer_pos(buffer, buffer_pos)
-            buffer = buffer[:p] + buffer[buffer_pos:]
+            buffer.delete_forward()
 
         elif printable is True:
-            buffer = buffer[:buffer_pos] + c + buffer[buffer_pos:]
-            buffer_pos += 1
+            buffer.add(c)
 
     window.move( max_y - 1, 0 );
     window.clrtoeol()
     window.refresh()
-    return buffer
-
-def next_buffer_pos(buffer, buffer_pos):
-    """Return next non combining character index from buffer_pos in buffer"""
-    l = len(buffer)
-    while buffer_pos < l:
-        buffer_pos += 1
-        if buffer_pos < l and unicodedata.combining(buffer[buffer_pos]):
-            continue
-        break
-    return buffer_pos
-
-def prev_buffer_pos(buffer, buffer_pos):
-    """Return last non combining character index from buffer_pos in buffer"""
-    while True:
-        if buffer_pos == 0:
-            return buffer_pos
-        buffer_pos -= 1
-        if unicodedata.combining(buffer[buffer_pos]):
-            continue
-        else:
-            return buffer_pos
-
+    if buffer:
+        return buffer.to_string()
 
 class Buffer():
 
